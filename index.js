b5eba9fcf8d15aaae40efbf58e3a426bc3341506
Heloisa <mariahelo@yahoo.com.br>
2024-08-13 12:37:12 -0300

    Criando páginas com handlebars

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const http = require('http');
-const hostname ='127.0.0.1';//Endereço do servidor
-const port = 3000;// porta do servidor:
-const server = http.createServer((req,res)=> {// Criando o servidor
-    res.statusCode =200;// Status ok de resposta
-    res.setHeader('Content-Type', 'text/plain');
-res.end ('Olá, sou Heloisa, tenho 58 anos, aluna do curso de Desenvolvedor de Sistemas pelo Instituto Federal de Ubá. Embora o curso seja difícil e estou gostando muito.');
-});
-server.listen(port, hostname,()=>{ 
-    console.log('/servidor rodando em http://${hostname}:${port}/');
+const express= require("express");
+const app = express();
+const path=require ('path');//endereço de cada rota
+const router=express.Router();//verificando comas rotas
+
+const {engine} = require("express-handlebars");
+
+app.engine('handlebars', engine ({
+    defaultLayout:'main',
+    runtimeOptions:{
+        allowProtoPropertiesByDefault: true,
+        allowProtoMethodsByDefault: true,
+    },
+}))
+app.set('view engine', 'handlebars');
+router.get('/', function (req,res){
+    res.render('index');
+})
+router.get('/about',function(req,res){
+    res.sendFile(path.join(__dirname+'/about.html'));
 })
